154bcb368bb3222359d9d0f7ba49c8de292818b8
Aleem Isiaka <aleemisiaka@gmail.com>
2020-03-24 10:47:07 +0100

    Sizing of containers and sections

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,21 @@
 import React from "react"
+import "./index.css"
 // import { Link } from "gatsby"
 
 // import Layout from "../components/layout"
 // import Image from "../components/image"
 // import SEO from "../components/seo"
 
-const IndexPage = () => <div>The main page</div>
+const IndexPage = () => (
+  <>
+    <div className="top-bar"></div>
+    <div className="app-container">
+      <div class="sidebar">
+        loremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremlorem
+      </div>
+      <div class="content-section"></div>
+    </div>
+  </>
+)
 
 export default IndexPage
