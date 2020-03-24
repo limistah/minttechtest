a09fa524413a96f4e02d5b377a71a37a7a8584ee
Aleem Isiaka <aleemisiaka@gmail.com>
2020-03-24 12:23:35 +0100

    Completes searchbar

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -1,11 +1,15 @@
 import React from "react"
 import Logo from "../components/Logo"
+import Searchbar from "../components/Searchbar"
 
 function Main() {
   return (
     <>
       <div className="top-bar">
         <Logo />
+        <div className="top-bar-container">
+          <Searchbar />
+        </div>
       </div>
       <div className="app-container">
         <div class="sidebar">
