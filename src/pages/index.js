35b0a37867f2b13ba66fc13495098f87d6039cd2
Aleem Isiaka <aleemisiaka@gmail.com>
2020-03-24 09:40:26 +0100

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+// import { Link } from "gatsby"
+
+// import Layout from "../components/layout"
+// import Image from "../components/image"
+// import SEO from "../components/seo"
+
+const IndexPage = () => <div>The main page</div>
+
+export default IndexPage
